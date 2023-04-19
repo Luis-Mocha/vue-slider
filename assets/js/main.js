3bca023573d5439a59c5470282b140f7c84587e2
Luis-Mocha <luismocha97@gmail.com>
2023-04-19 16:59:41 +0200

    vue setting

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const {createApp} = Vue
+
+createApp({
+    data() {
+        return {
+            // variabili Vue3
+            
+        }
+    },
+
+    
+    created() { 
+        //cicli di vita dei componenti di applicazioni Vue. Vengono eseguite azioni al caricamento dei componenti
+        
+    },
+
+    methods: {
+        // funzioni Vue3
+        
+
+    }
+}).mount('#app')
